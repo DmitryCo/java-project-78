ac0b4efe9398f034e39a602a425c68d6eee17548
Dmitry Konstantinov <dm3ypost@gmail.com>
2024-10-13 23:49:01 +0300

    Add StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,10 +1,17 @@
 package hexlet.code.schemas;
 
-public final class StringSchema {
+import java.util.Objects;
+
+public final class StringSchema extends BaseSchema<String> {
 
     public StringSchema() {
         addCheck("isAllowedAndEmpty", s -> (s != null) && (!required || !s.isEmpty()));
         addCheck("isString", Objects::nonNull);
+    }
+
+    public StringSchema required() {
+        required = true;
+        return this;
     }
 
     public StringSchema minLength(int lengthOfString) {
@@ -12,13 +19,8 @@ public final class StringSchema {
         return this;
     }
 
-    public StringSchema contains(String restriction) {
-        addCheck("contains", s -> ((String) s).contains(restriction));
-        return this;
-    }
-
-    public StringSchema required() {
-        required = true;
+    public StringSchema contains(String contentLimit) {
+        addCheck("contains", s -> ((String) s).contains(contentLimit));
         return this;
     }
 }
