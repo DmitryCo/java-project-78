1c19faba6bbbfa3b5190b79e44185a7f1be40e82
Dmitry Konstantinov <dm3ypost@gmail.com>
2024-10-13 23:50:16 +0300

    Add tests for StringSchema

---
+++ app/src/test/java/hexlet/code/schemasTest/StringSchemaTest.java
@@ -0,0 +1,23 @@
+package hexlet.code.schemas;
+
+import hexlet.code.Validator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringSchemaTest {
+    @Test
+    public void testString() {
+        Validator v = new Validator();
+        StringSchema schema = v.string();
+        assertThat(schema.isValid("")).isTrue();
+        assertThat(schema.isValid(null)).isTrue();
+        schema.required();
+        assertThat(schema.isValid("")).isFalse();
+        assertThat(schema.isValid(null)).isFalse();
+        assertThat(schema.isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains("wh").isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains("whattle").isValid("what does the fox say")).isFalse();
+        assertThat(schema.isValid("what does the fox say")).isFalse();
+    }
+}
