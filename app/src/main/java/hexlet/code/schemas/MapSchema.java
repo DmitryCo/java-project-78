7f2946547846fb97fcfd44fed1ca0dfda39c0229
Dmitry Konstantinov <dm3ypost@gmail.com>
2024-10-16 01:09:45 +0300

    Add MapSchema

---
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -0,0 +1,28 @@
+package hexlet.code.schemas;
+
+import java.util.Map;
+
+public final class MapSchema<T> extends BaseSchema<Map<String, T>> {
+
+    public MapSchema shape(Map<String, BaseSchema<T>> schemas) {
+        addCheck("shape", map -> {
+            for (Map.Entry<String, BaseSchema<T>> entry : schemas.entrySet()) {
+                if (!entry.getValue().isValid(map.get(entry.getKey()))) {
+                    return false;
+                }
+            }
+            return true;
+        });
+        return this;
+    }
+
+    public MapSchema required() {
+        required = true;
+        return this;
+    }
+
+    public MapSchema sizeof(Integer size) {
+        addCheck("mapSize", m -> m.size() == size);
+        return this;
+    }
+}
