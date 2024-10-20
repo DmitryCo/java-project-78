a69fa4b85283ebaf88019aaefb63b54a58f0ee4d
Dmitry Konstantinov <dm3ypost@gmail.com>
2024-10-20 14:16:20 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,15 +20,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("org.apache.commons:commons-collections4:4.4")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.1")
     testImplementation("org.assertj:assertj-core:3.21.0")
-    testImplementation("org.skyscreamer:jsonassert:1.5.0")
-    implementation("info.picocli:picocli:4.7.6")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
 }
 
 tasks.test {
